895441d6fa099acbe699bcd5098f8c09f4ec76e7
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2023-03-30 09:39:02 +0100

    add vitest and eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log('LLLLLLL');
+import { app } from './app';
+import { getSettings } from './settings';
+
+const settings = await getSettings();
+
+try {
+  await app.listen({ port: settings.port });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
