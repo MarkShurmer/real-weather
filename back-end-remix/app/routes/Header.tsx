d4b9ae120e8bf5c7a51cb540fc81e841f08a1e3a
Mark Shurmer <14219694+MarkShurmer@users.noreply.github.com>
2025-02-03 10:20:27 +0000

    put install back in actions

--- back-end-remix/app/routes/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <section className="sticky top-0 mx-auto h-12 bg-gray-600 text-white">
-      Header
-    </section>
-  );
-}
